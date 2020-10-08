8c4e090eddd9d503f48fedc3adf85eaf17e3803a
Ricardo Watanabe Bento <roicoroy@yahoo.com.br>
2020-10-08 08:35:04 +0100

    first

---
+++ index.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// Express route for incoming requests
+app.get('/goiaba/message/', function(req, res) {
+  res.status(200).send('hello from server');
+});
+
+app.get('/goiaba/led/', function(req, res) {
+  res.status(200).send('goiaba led');  
+});
+
+// Express route for any other unrecognised incoming requests
+app.get('*', function(req, res) {
+  res.status(404).send('Unrecognised API call');
+});
+
+// Express route to handle errors
+app.use(function(err, req, res, next) {
+  if (req.xhr) {
+    res.status(500).send('Oops, Something went wrong!');
+  } else {
+    next(err);
+  }
+});
+
+app.listen(5000);
