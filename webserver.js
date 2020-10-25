2dcad78c457172a22e8ec57d3dcd6ad7ae90704e
Ricardo Watanabe Bento <roicoroy@yahoo.com.br>
2020-10-25 20:21:56 +0000

    d

--- webserver.js
+++ webserver.js
@@ -10,43 +10,33 @@ var pushButton = new Gpio(26, 'in', 'both');
 
 http.listen(5000);
 
-// app.use(function (req, res, next) {
-//  res.header("Access-Control-Allow-Origin", "*");
-//  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//  next();
-// });
-
-function handler (req, res) { //create server
-    const headers = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
-      "Access-Control-Max-Age": 2592000, // 30 days
-      /** add other headers as per requirement */
-    };
-  fs.readFile(__dirname + '/public/index.html', function(err, data) { //read file index.html in public folder
+function handler(req, res) { //create server
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+    "Access-Control-Max-Age": 2592000, // 30 days
+    /** add other headers as per requirement */
+  };
+  fs.readFile(__dirname + '/public/index.html', function (err, data) { //read file index.html in public folder
     if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'}, headers); //display 404 on error
+      res.writeHead(404, { 'Content-Type': 'text/html' }, headers); //display 404 on error
       return res.end("404 Not Found");
     }
-    res.writeHead(200, {'Content-Type': 'text/html'}, headers); //write HTML
+    res.writeHead(200, { 'Content-Type': 'text/html' }, headers); //write HTML
     res.write(data); //write data from index.html
     return res.end();
   });
 }
-// Express route for incoming requests
 
 io.sockets.on('connection', function (socket) {   // WebSocket Connection
   var lightvalue = 0;
-                                                  // static variable for current status
-
-  socket.on('light', function(data) {             // get light switch status from client
+  socket.on('light', function (data) {
     lightvalue = data;
-    if (lightvalue != LED.readSync()) {           // only change LED if status has changed
-      LED.writeSync(lightvalue);                  // turn LED on or off
-      console.log("lightvalue : " + lightvalue); 
+    if (lightvalue != LED.readSync()) {
+      LED.writeSync(lightvalue);
+      console.log("lightvalue : " + lightvalue);
       socket.emit('light', lightvalue);
     }
-
   });
 });
 
